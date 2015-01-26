e713e5e673c2031275fe5d4d9ff6598ebabfddd3
Aijaz A. Ansari <aijaz@enoor.com>
2015-01-26 10:07:46 -0600

    Working with swipes

--- Robots/ViewController.m
+++ Robots/ViewController.m
@@ -51,16 +51,148 @@
     
     
     [self.arenaView setColumns:self.arena.width rows:self.arena.height];
-    [self.arenaView setPlayerLocation:[[ItemLocation alloc] initWithColumn:self.arena.playerStartX row:self.arena.playerStartY]];
+    [self.arenaView setPlayerLocation:[[    ItemLocation alloc] initWithColumn:self.arena.playerStartX row:self.arena.playerStartY]];
 
     [self translateRobotLocationsFromModelToView];
     [self translateDebrisLocationsFromModelToView];
     
     self.arenaView.gameOver = NO;
     
+    // add gesture recognizers
+    UIPanGestureRecognizer * pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+    [self.view addGestureRecognizer:pan];
+    UITapGestureRecognizer * tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+    [self.arenaView addGestureRecognizer:tap];
+    UILongPressGestureRecognizer * longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
+    [self.arenaView addGestureRecognizer:longPress];
+    
+    
     [self refreshScreen];
 
 }
+
+#pragma mark - Gesture Recognizers
+
+-(void) pan: (UIPanGestureRecognizer *) pan {
+    switch (pan.state) {
+        case UIGestureRecognizerStateBegan: {
+            
+            break;
+        }
+            
+        case UIGestureRecognizerStateChanged:{
+//            CGPoint translation = [pan translationInView:self.view];
+//            NSInteger spot = [self spotFromCGPoint:translation];
+            break;
+        }
+        case UIGestureRecognizerStateEnded: {
+            CGPoint translation = [pan translationInView:self.view];
+            NSInteger spot = [self spotFromCGPoint:translation];
+            [self handleMoveRequestToSpot: spot];
+        }
+        default:
+            break;
+    }
+}
+
+
+-(void) tap: (UITapGestureRecognizer *) tap {
+    [self moveToSpot:5];
+}
+
+
+-(void) longPress: (UILongPressGestureRecognizer *) longPress {
+}
+
+
+-(NSInteger) spotFromCGPoint: (CGPoint) point {
+    CGFloat radians = atan2f(-point.y, point.x);
+    CGFloat degrees = ((radians * 180) / M_PI);
+    if (degrees < 0) { degrees = 360 + degrees; }
+    NSLog(@"degrees is %.1f", degrees);
+
+    if (degrees < 0 + 22.5) {
+        return 6;
+    }
+    else if (degrees <= 45 + 22.5) {
+        return 3;
+    }
+    else if (degrees <= 90 + 22.5) {
+        return 2;
+    }
+    else if (degrees <= 135 + 22.5) {
+        return 1;
+    }
+    else if (degrees <= 180 + 22.5) {
+        return 4;
+    }
+    else if (degrees <= 225 + 22.5) {
+        return 7;
+    }
+    else if (degrees <= 270 + 22.5) {
+        return 8;
+    }
+    else if (degrees < 315 + 22.5) {
+        return 9;
+    }
+    else {
+        return 6;
+    }
+}
+
+-(void) handleMoveRequestToSpot: (NSInteger) spot {
+    NSDictionary * validMoves = [self.arena validMoves];
+    if (validMoves[@(spot)]) {
+        // it's a valid move
+        [self moveToSpot:spot];
+    }
+    else {
+        [self shakeArenaView];
+    }
+}
+
+-(void) moveToSpot: (NSInteger) spot {
+    [self.arena movePlayerToSpot:spot];
+    if (self.arena.player.isDead) {
+        self.arenaView.gameOver = YES;
+    }
+    [self translateFromModelToView];
+
+}
+
+-(void) shakeArenaView {
+    [UIView animateWithDuration:0.1 animations:^{
+        CGRect f = self.arenaView.frame;
+        f.origin.x -= 10;
+        self.arenaView.frame = f;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.1 animations:^{
+            CGRect f = self.arenaView.frame;
+            f.origin.x += 20;
+            self.arenaView.frame = f;
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:0.1 animations:^{
+                CGRect f = self.arenaView.frame;
+                f.origin.x -= 20;
+                self.arenaView.frame = f;
+            } completion:^(BOOL finished) {
+                [UIView animateWithDuration:0.1 animations:^{
+                    CGRect f = self.arenaView.frame;
+                    f.origin.x += 20;
+                    self.arenaView.frame = f;
+                } completion:^(BOOL finished) {
+                    [UIView animateWithDuration:0.1 animations:^{
+                        CGRect f = self.arenaView.frame;
+                        f.origin.x -= 10;
+                        self.arenaView.frame = f;
+                    }];
+                }];
+            }];
+        }];
+    }];
+}
+
+#pragma mark - Translations from model to view
 
 -(void) translateRobotLocationsFromModelToView {
     NSMutableSet * robotLocations = [NSMutableSet setWithCapacity:[self.arena.robots count]];
